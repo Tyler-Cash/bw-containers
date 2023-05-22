86a9742842c86b1cc1a745a32b4efb56d5ab956d
Christopher Larivière <lariviere.c@gmail.com>
2023-05-22 11:20:25 -0400

    add unlock check
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- apps/bitwarden-cli/entrypoint.sh
+++ apps/bitwarden-cli/entrypoint.sh
@@ -6,6 +6,7 @@ bw config server ${BW_HOST}
 
 bw login --apikey --raw
 export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
+bw unlock --check
 
 echo 'Running `bw server` on port 8087'
 bw serve --hostname 0.0.0.0 #--disable-origin-protection
